a0415267d3a510d5bc1ff0dcd6eebe324c545e55
Ziyoda <Ziyodaeva@mail.com>
2023-05-15 16:06:24 +0500

    added templates/food

--- config/admin.py
+++ config/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(ContactLink)
+admin.site.register(ContactModel)
+admin.site.register(About)
+admin.site.register(Social)
+admin.site.register(ImageAbout)
+
